f1f57a4e29c2c9dde8884ea6f9ee645edf0800ab
RidvanOzturk <ozturkridvan1@gmail.com>
2023-12-04 11:28:28 +0300

    edited addFile page and added datepicker

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,6 +3,15 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
+    
+    /*<footer className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
+    <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
+      © 2023 Copyright:
+      <a
+        className="text-neutral-800 dark:text-neutral-400"
+        href="https://tailwind-elements.com/"
+      >TW Elements</a>
+    </div>*/
     <footer>
         <Link to="/login" className="">Login</Link>
          &nbsp;
