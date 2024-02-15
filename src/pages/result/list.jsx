f288530f538003a776f13cb5f526af120a3776f4
RidvanOzturk <ozturkridvan1@gmail.com>
2024-02-15 12:47:55 +0300

    flowbite test

--- src/pages/result/list.jsx
+++ src/pages/result/list.jsx
@@ -39,12 +39,9 @@ function List() {
    <br/>
    <Card href="#" className="max-w-sm">
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        Noteworthy technology acquisitions 2021
+      <h3 className='text-center text-lg font-bold'>Yüklenen Dosyalar</h3>
       </h5>
       <p className="font-normal text-gray-700 dark:text-gray-400">
-        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-      </p>
-    </Card>
       {
         data && data.map((value) =>
           <>
@@ -53,6 +50,9 @@ function List() {
           </>
         )
       }
+      </p>
+    </Card>
+     
     </>
   );
 }
