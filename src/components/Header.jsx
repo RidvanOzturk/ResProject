5da09414996048d11cbe59d24b19a69b70966129
RidvanOzturk <ozturkridvan1@gmail.com>
2024-02-25 14:00:16 +0300

    added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import {useSelector} from "react-redux";
 import useAuthentication from "../hooks/useAuthentication";
 import { NavLink, useNavigate } from "react-router-dom";
 import { RoleTypes } from "../RoleTypes";
+import logo from "../assets/uu_logo.png"
 
 const Header = () => {
 
@@ -36,15 +37,19 @@ const Header = () => {
 
   return (
     
-     <nav className="bg-cyan-500 border-white px-4 lg:px-6 py-2.5 dark:bg-cyan-500">
-            <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+     <nav className="bg-cyan-500 border-white px-4 lg:px-6  dark:bg-cyan-500">
+        <div class="flex-none w-14 h-5 ml-7 pt-3 mb-1">
+     <img src={logo} />
+   </div>
+            <div className="flex flex-wrap justify-between pb-4 items-center mx-auto max-w-screen-xl">
                 {
                    user.username
                     ?
                     <>
-                    <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                    <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                    <div className="hidden justify-between items-center w-full lg:flex lg:w-auto  lg:order-1" id="mobile-menu-2">
+                    <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 ">
                     {
+                        
                         user.role==RoleTypes.admin
                         ?
                         <>
@@ -72,13 +77,12 @@ const Header = () => {
                         <div className="flex items-center lg:order-2 ">
                             <button type="button"
                             onClick={handleLogout}
-                            className="text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2   ">
+                            className="text-white hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2   ">
                                 Çıkış
                             </button>
                         </div>
                     </>
-                    : 
-                    null
+                    : null
                 }
                 
             </div>
--- src/pages/result/list-detail.jsx
+++ src/pages/result/list-detail.jsx
@@ -133,7 +133,7 @@ const ListDetail = () => {
                 isTableLoading ? <LoadingSpinner /> :
                 docData && tableData &&
                 <div className="relative overflow-x-auto">
-                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <table className="w-full text-sm mt-7 text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" className="px-6 py-3">
