a8c9379f2a82b2b36b4b5be16b8bf35aab2fc545
RidvanOzturk <ozturkridvan1@gmail.com>
2024-03-13 16:03:43 +0300

    added Tailwind pagination and will fix pagination bugs

--- src/pages/result/list.jsx
+++ src/pages/result/list.jsx
@@ -11,6 +11,8 @@ import {
 import { firestore } from "../../firebase";
 import { useSelector } from "react-redux";
 import { Card } from "flowbite-react";
+import { Button, IconButton } from "@material-tailwind/react";
+import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { RoleTypes } from "../../RoleTypes";
 import { Link, useLocation} from "react-router-dom";
 import Swal from "sweetalert2";
@@ -72,7 +74,26 @@ function List() {
       console.log('Error copying to clipboard:', error);
     }
   };
-
+  const [active, setActive] = useState(1);
+ 
+  const getItemProps = (index) =>
+    ({
+      variant: active === index ? "filled" : "text",
+      color: "gray",
+      onClick: () => setActive(index),
+    });
+ 
+  const next = () => {
+    if (active === 5) return;
+ 
+    setActive(active + 1);
+  };
+ 
+  const prev = () => {
+    if (active === 1) return;
+ 
+    setActive(active - 1);
+  };
   return (
     <>
       <h1 className="text-center text-2xl font-bold">List Page</h1>
@@ -101,6 +122,32 @@ function List() {
           Toplam dosya sayısı
           <h1>{data && data.length}</h1>
         </p>
+        <div className="flex items-center justify-center gap-4">
+      <Button
+        variant="text"
+        className="flex items-center gap-2"
+        onClick={prev}
+        disabled={active === 1}
+      >
+        <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" /> Previous
+      </Button>
+      <div className="flex items-center gap-2">
+        <IconButton {...getItemProps(1)}>1</IconButton>
+        <IconButton {...getItemProps(2)}>2</IconButton>
+        <IconButton {...getItemProps(3)}>3</IconButton>
+        <IconButton {...getItemProps(4)}>4</IconButton>
+        <IconButton {...getItemProps(5)}>5</IconButton>
+      </div>
+      <Button
+        variant="text"
+        className="flex items-center gap-2"
+        onClick={next}
+        disabled={active === 5}
+      >
+        Next
+        <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
+      </Button>
+    </div>
       </Card>
     </>
   );
